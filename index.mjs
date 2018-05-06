4d9a89292b9d9f0d4b2cba5c174053745861aaf7
Shutaro Takimoto <shu@tak.im>
2018-05-06 22:21:22 +0900

    refactor: レンダラを分けた

--- index.mjs
+++ index.mjs
@@ -1,17 +1,16 @@
 'use strict';
-import chalk from 'chalk';
-
 import SemantrisAPI from './api';
 import SemantrisGameState from './state';
 import SemantrisGameUpdater from './updater';
 import SemantrisParamTuner from './paramTuner';
+import ttyRenderer from './ttyRenderer';
 import {FB, GAMEMODE_ARCADE, GAMEMODE_BLOCKS} from './define';
 import * as util from './util';
 
-
 class Semantris {
-    constructor(updater = SemantrisGameUpdater, tuner = SemantrisParamTuner) {
+    constructor(renderer = ttyRenderer, updater = SemantrisGameUpdater, tuner = SemantrisParamTuner) {
         this.api = new SemantrisAPI();
+        this.renderer = renderer;
         this.updater = new updater({
             paramTuner: tuner,
             wordSelector: this.selectWord.bind(this)
@@ -40,7 +39,7 @@ class Semantris {
     reset(gameMode = GAMEMODE_ARCADE) {
         this.state = null;
         const feedback = this.update('init', gameMode);
-        this.view(this.state, 'init', feedback);
+        this.renderer(this.state, 'init', feedback);
         this._mainLoop().then(); // 初期化が終わったらメインループ開始
         
         return this;
@@ -54,7 +53,7 @@ class Semantris {
                 continue; // 通信中は tick しない
             }
             feedback = this.update('tick');
-            this.view(this.state, 'tick', feedback);
+            this.renderer(this.state, 'tick', feedback);
         }
     }
 
@@ -66,96 +65,7 @@ class Semantris {
         this.connecting = false;
 
         const feedback = this.update('input', matchResult);
-        this.view(this.state, 'input', feedback);
-    }
-
-    view(state, action, feedback) {
-        if (feedback === FB.TICK) {
-            return;
-        }
-        
-        process.stdout.write('\x1b[2J');
-        process.stdout.write('\x1b[0f');
-
-        // debug
-        /*
-        if (action) {
-            console.log(chalk.yellow('(' + action + ')') + ' => ' +
-                        chalk.magenta(feedback));
-        } else {
-            console.log('');
-        }
-        */
-        
-        const WIDTH = 24;
-        const LEFT = 16;
-        const pad = Array(LEFT + 1).join(' ');
-        let buffer = '';
-        function write(str = '') {
-            buffer += str.split('\n')
-                         .map(line => pad + line)
-                         .join('\n') + '\n';
-        }
-
-        // ワード
-        (function renderWords() {
-            const renderWord = (word, bg, fore) => {
-                const spaces = Array(WIDTH + 1).join(' ');
-                const padding = (word.word + spaces).slice(0, WIDTH);
-                write(bg(fore(padding)));
-            };
-
-            for (let i = state.dieBorder; i >= 0; --i) {
-                const w = state.candidates[i];
-                if (w) {
-                    const bg = i < state.targetBorder ?
-                            chalk.bgBlackBright : chalk.bgBlack;
-                    const fore = state.targetIndexes.includes(i) ?
-                                chalk.cyan : chalk.white;
-                    renderWord(w, bg, fore);
-                } else {
-                    write();
-                }
-            }
-        })();
-
-        // ゲージ
-        (function renderStreakGauge() {
-            const unitLen = Math.round(WIDTH / state.streakMax);
-            const unit = '[' + Array(unitLen - 1).join(' ') + ']';
-
-            write();
-            let s = '';
-            for (let i = 0; i < state.streakMax; ++i) {
-                if (i < state.streakProgress) {
-                    s += chalk.bgBlue(unit);
-                } else {
-                    s += unit;
-                }
-            }
-            write(s);
-        })();
-
-        if (feedback === FB.TICK_DIE) {
-            write(chalk.bgRed(chalk.white('       GAME  OVER       ')));
-            write(chalk.green('SCORE = ' + state.score));
-        }
-        console.log(buffer);
-
-        // 演出・ウェイト
-        switch (feedback) {
-            case FB.INPUT_FAIL:
-                break;
-            case FB.INPUT_SUCCESS:
-            case FB.DESTROY_NORMAL:
-                break;
-            case FB.DESTROY_STREAK:
-                break;
-            default:
-                break;
-        }
-
-        process.stdout.write(pad + '> '); // prompt
+        this.renderer(this.state, 'input', feedback);
     }
 
     filterWord(levels, excludes = []) {
@@ -195,7 +105,7 @@ new Semantris().start().then(async (game) => {
         if (input) {
             await game.input(input);
         } else {
-            game.view(game.state);
+            game.renderer(game.state);
         }
     }
     process.exit();
---
+++ ttyRenderer.mjs
@@ -0,0 +1,95 @@
+import chalk from 'chalk';
+import {FB} from './define';
+
+const WIDTH = 24;
+const LEFT = 16;
+const pad = Array(LEFT + 1).join(' ');
+let buffer = '';
+
+function clearTTY() {
+    process.stdout.write('\x1b[2j');
+    process.stdout.write('\x1b[0f');
+}
+
+function write(str = '') {
+    buffer += str.split('\n')
+                 .map(line => pad + line)
+                 .join('\n') + '\n';
+}
+
+function flush() {
+    console.log(buffer);
+}
+
+function renderWords(state) {
+    const renderWord = (word, bg, fore) => {
+        const spaces = Array(WIDTH + 1).join(' ');
+        const padding = (word.word + spaces).slice(0, WIDTH);
+        write(bg(fore(padding)));
+    };
+
+    for (let i = state.dieBorder; i >= 0; --i) {
+        const w = state.candidates[i];
+        if (w) {
+            const bg = i < state.targetBorder ?
+                    chalk.bgBlackBright : chalk.bgBlack;
+            const fore = state.targetIndexes.includes(i) ?
+                        chalk.cyan : chalk.white;
+            renderWord(w, bg, fore);
+        } else {
+            write();
+        }
+    }
+    write();
+}
+
+function renderStreakGauge(state) {
+    const unitLen = Math.round(WIDTH / state.streakMax);
+    const unit = '[' + Array(unitLen - 1).join(' ') + ']';
+
+    let s = '';
+    for (let i = 0; i < state.streakMax; ++i) {
+        if (i < state.streakProgress) {
+            s += chalk.bgBlue(unit);
+        } else {
+            s += unit;
+        }
+    }
+    write(s);
+}
+
+function renderStatusBar(state, feedback) {
+    if (feedback === FB.TICK_DIE) {
+        write(chalk.bgRed(chalk.white('       GAME  OVER       ')));
+    } else {
+        write();
+    }
+    write(chalk.green('SCORE: ' + state.score));
+}
+
+function renderDebug(state, action, feedback) {
+    if (action) {
+        console.log(chalk.yellow('(' + action + ')') + ' => ' +
+                    chalk.magenta(feedback));
+    } else {
+        console.log('');
+    }
+}
+
+function renderEffect(state) {
+    // TODO:
+}
+
+export default function ttyRenderer(state, action, feedback) {
+    if (feedback === FB.TICK) {
+        return;
+    }
+
+    clearTTY();
+    // renderDebug(state, action, feedback);
+    renderWords(state);
+    renderStreakGauge(state);
+    renderStatusBar(state, feedback);
+    renderEffect(state);
+    flush();
+}
