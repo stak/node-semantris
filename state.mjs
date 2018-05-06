d94754aface6a92ca75e8e30b15aba9560ad84f9
Shutaro Takimoto <shu@tak.im>
2018-05-06 14:45:55 +0900

    fix: makeWord() が壊れていてワード追加に失敗していた

--- state.mjs
+++ state.mjs
@@ -50,7 +50,7 @@ export default class SemantrisGameState {
     }
 
     makeWord() {
-        return this.wordSelector(1, this.wordLevel, this.candidates.map(c => c.word));
+        return this.wordSelector(1, this.wordLevel, this.candidates.map(c => c.word))[0];
     }
     makeInitialWords() {
         return this.wordSelector(this.fillBorder, this.wordLevel);
